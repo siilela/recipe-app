55d3096ca73b34064c257ff54841a0babbd9b627
sumogashoa <sumogashoa@shoprite.co.za>
2022-03-14 11:22:00 +0200

    recipe app commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Category from "./components/Category";
+import Search from "./components/Search";
+import Pages from "./pages/Pages";
+import {BrowserRouter} from 'react-router-dom';
+import styled from 'styled-components';
+import { AiFillAlipayCircle } from "react-icons/ai";
+import {GiKnifeFork} from "react-icons/gi";
+import {Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Nav>
+        <AiFillAlipayCircle/>
+        <Logo to={"/"}>Delicous Sii</Logo>
+      </Nav>
+      <Search/>
+      <Category/>
+      <Pages/></BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
+const Logo=styled(Link)`
+            text-decoration:none;
+            font-size:1.5rem;
+            font-weight:400;
+            font-family:'Lobster Two', cursive;
+            `
+const Nav=styled.div`
+                 padding:4rem 0rem;
+                 display:flex;
+                 justify-content:flex-start;
+                 align-item:center;
+                 svg{
+                   font-size:2rem;
+                 }`
+                 
